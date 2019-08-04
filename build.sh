20333a97a0bf4d50c0962abe0c2e1e8b5f0f3d96
ljx95 <38516606+ljx95@users.noreply.github.com>
2019-08-04 20:19:27 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-
+echo "查询标记"
 # 停止容器
-sudo docker stop demo-jenkins
+docker stop demo-jenkins
 
 # sleep 2s
 
 # 删除容器
-sudo docker rm demo-jenkins
+docker rm demo-jenkins
 
 # 删除镜像
-sudo docker rmi demo-jenkins/demo:1.0
+docker rmi demo-jenkins/demo:1.0
 
 # 构建镜像 . 号表示在当前目录构建
-sudo docker build -t demo-jenkins/demo:1.0 .
+docker build -t demo-jenkins/demo:1.0 .
 
 # 创建容器并启动
-sudo docker run -d -p 8888:8888 --name demo-jenkins demo-jenkins/demo:1.0
+docker run -d -p 8888:8888 --name demo-jenkins demo-jenkins/demo:1.0
 
 #!/bin/bash
 #IMAGE=demo-jenkins
