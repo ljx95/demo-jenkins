97af0ad9af23222507227913f29ff70e39a35d7e
ljx95 <38516606+ljx95@users.noreply.github.com>
2019-08-04 20:58:04 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,20 +4,19 @@ echo "查询标记"
 docker stop demo-jenkins
 
 # sleep 2s
-
+echo "删除容器"
 # 删除容器
 docker rm demo-jenkins
-
+echo "删除镜像"
 # 删除镜像
 docker rmi demo-jenkins/demo:1.0
-
+echo "构建镜像"
 # 构建镜像 . 号表示在当前目录构建
 docker build -t demo-jenkins/demo:1.0 .
-
+echo "创建容器并启动"
 # 创建容器并启动
 docker run -d -p 8888:8888 --name demo-jenkins demo-jenkins/demo:1.0
 
-#!/bin/bash
 #IMAGE=demo-jenkins
 #docker stop myapp
 #sleep 3s
